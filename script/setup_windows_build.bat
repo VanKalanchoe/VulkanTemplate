f7e870a4cb55ddfe3265cda98fcb212a04ff7651
VanKalanchoe <145471180+VanKalanchoe@users.noreply.github.com>
2025-09-20 18:45:41 +0200

    Initial setup
    clones vulkan tutorial stripped everything from chapters just a main to see if everything is correctly setup

---
+++ script/setup_windows_build.bat
@@ -0,0 +1,8 @@
+:: Add local vcpkg folder to PATH temporarily
+set "VCPKG_ROOT=%~dp0..\vcpkg"
+set "PATH=%VCPKG_ROOT%;%PATH%"
+
+cmake -B ../build -S .. -DCMAKE_TOOLCHAIN_FILE=%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake
+:: cmake --build build
+
+pause
